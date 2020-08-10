60387a3dca3e1b82a9e71109893942235de1ca85
Regan Layman <regan.layman@gmail.com>
2020-08-10 12:37:17 -0400

    defined method print_list and instructed it to print the list defined earlier using the method create_list. used string interpolation to print list name, item, and and quantity. Used an EACH method to apply directive to each item in the list

--- shopping_list.rb
+++ shopping_list.rb
@@ -17,8 +17,22 @@ def add_list_item
   return hash
 end
 
+def print_list(list) #method with list we want to print as argument
+  puts "List; #{list['name']}" #print list name with string interpolation
+  puts "----" #makes a line
+  
+  list["items"].each do |item| #each method - blocks are curly braces or 'do' and 'end'
+    #take each item in this array of list items and assign it to a variable called 'item'
+    puts "Item: " + item['name'] #print out item name 
+    puts "Quantity: " + item['quantity'] #and quantity
+    puts "----" #makes a line
+  end
+end
+
 list = create_list()
 puts list.inspect
 list['items'].push(add_list_item())
+
+puts list.inspect
 
 puts add_list_item().inspect
